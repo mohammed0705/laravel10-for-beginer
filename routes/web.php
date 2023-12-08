86dfdf66fb9b0f50e52fafffae83deb9d3260a07
mohammed0705 <djellouli256@gmail.com>
2023-12-08 22:56:32 +0100

    quiree

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,5 +15,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
-Route::view('/mmm', 'home');
+Route::get('/', function () {
+   // return view('welcome');
+   //$users = DB::delete('delete from users where name ="mohammed"');
+   $users = DB::select('select * from users');
+   /*$users =  DB::update(
+    'update users set id = 100 where name = ?',
+    ['mohammed']
+);*/
+
+   //$users=DB::insert('insert into users (name,email,password) values (?, ?,?)', [1, 'Marc@gmail.com','TT32']);
+   dd($users);
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
